c14d90a356933897848f68d9b95a8142c23c3418
Jason Zou <Jason.zou@gmail.com>
2018-03-06 16:29:51 -0500

    shameless borrow code from trellis-ldp/py-ldnlib
    https://github.com/trellis-ldp/py-ldnlib - no license found

---
+++ lib.py
@@ -0,0 +1,155 @@
+
+from rdflib import Graph, URIRef
+import requests
+
+
+class BaseLDN(object):
+
+    ACCEPT_HEADERS = ("application/ld+json; q=1.0,"
+                      "text/turtle; q=0.9,"
+                      "application/xml+rdf; q=0.5")
+    JSON_LD = "application/ld+json"
+    LDP_INBOX = "http://www.w3.org/ns/ldp#inbox"
+    LDP_CONTAINS = "http://www.w3.org/ns/ldp#contains"
+
+    def __init__(self, **kwargs):
+        self.accept_headers = kwargs.get('accept_headers', self.ACCEPT_HEADERS)
+
+    def __discover_head(self, target, **kwargs):
+        r = requests.head(target, **kwargs)
+        r.raise_for_status()
+        if self.LDP_INBOX in r.links:
+            return r.links[self.LDP_INBOX].get('url')
+
+    def __discover_get(self, target, **kwargs):
+        r = requests.get(target, **kwargs)
+        r.raise_for_status()
+        # TODO -- check for HTML
+        g = Graph().parse(data=r.text, format=self.content_type_to_mime_type(
+            r.headers['content-type']))
+
+        for (subject, inbox) in g[:URIRef(self.LDP_INBOX)]:
+            return str(inbox)
+
+    def content_type_to_mime_type(self, content_type):
+        """
+        A utility method to convert a content-type header into a
+        mime-type string.
+        """
+        return content_type.split(";")[0].strip()
+
+    def discover(self, target, **kwargs):
+        """Discover the inbox for a resource."""
+        headers = kwargs.pop("headers", dict())
+        if 'accept' not in headers:
+            headers['accept'] = kwargs.pop("accept", self.accept_headers)
+        allow_redirects = kwargs.pop('allow_redirects', True)
+
+        inbox = self.__discover_head(target, headers=headers,
+                                     allow_redirects=allow_redirects,
+                                     **kwargs)
+        if inbox is None:
+            return self.__discover_get(target, headers=headers,
+                                       allow_redirects=allow_redirects,
+                                       **kwargs)
+        else:
+return inbox
+
+from rdflib import Graph, URIRef
+import requests
+
+import json
+
+from .base import BaseLDN
+
+
+class Consumer(BaseLDN):
+
+    def __init__(self, **kwargs):
+        super(self.__class__, self).__init__(**kwargs)
+
+    def notifications(self, inbox, **kwargs):
+        """
+        Retrieve all of the notification IRIs from an ldp:inbox as a list.
+        """
+        headers = kwargs.pop("headers", dict())
+        if 'accept' not in headers:
+            headers['accept'] = kwargs.pop("accept", self.accept_headers)
+
+        r = requests.get(inbox, headers=headers, **kwargs)
+        r.raise_for_status()
+        g = Graph().parse(data=r.text, format=self.content_type_to_mime_type(
+            r.headers['content-type']))
+        return [str(o) for (s, o) in g[:URIRef(self.LDP_CONTAINS)]]
+
+    def notification(self, iri, **kwargs):
+        """
+        Retrieve a single LDN notification and decode into a Python object.
+        """
+        headers = kwargs.pop("headers", dict())
+        if 'accept' not in headers:
+            headers['accept'] = kwargs.pop("accept", self.accept_headers)
+
+        r = requests.get(iri, headers=headers, **kwargs)
+        r.raise_for_status()
+        mime_type = self.content_type_to_mime_type(r.headers['content-type'])
+        if mime_type == self.JSON_LD:
+            return r.json()
+        else:
+            g = Graph().parse(data=r.text, format=mime_type)
+            return json.loads(str(g.serialize(format="json-ld"), 'utf-8'))
+
+
+from rdflib import Graph
+import requests
+
+import ipaddress
+import json
+import socket
+from urllib.parse import urlparse
+
+from .base import BaseLDN
+
+
+class Sender(BaseLDN):
+
+    def __init__(self, **kwargs):
+        super(self.__class__, self).__init__(**kwargs)
+        self.allow_localhost = kwargs.get('allow_localhost', False)
+
+    def __accept_post_options(self, inbox, **kwargs):
+        r = requests.options(inbox, **kwargs)
+        if r.status_code == requests.codes.ok and 'accept-post' in r.headers:
+            if self.JSON_LD in r.headers['accept-post']:
+                return self.JSON_LD
+
+            for content_type in r.headers['accept-post'].split(','):
+                return self.content_type_to_mime_type(content_type)
+
+    def __is_localhost(self, inbox):
+        return ipaddress.ip_address(socket.gethostbyname(
+            urlparse(inbox).hostname)).is_loopback
+
+    def __post_message(self, inbox, data, content_type, **kwargs):
+        if self.allow_localhost or not self.__is_localhost(inbox):
+            headers = kwargs.pop("headers", dict())
+            headers['content-type'] = content_type
+            r = requests.post(inbox, data=data, headers=headers, **kwargs)
+            r.raise_for_status()
+        else:
+            raise ValueError("Invalid local inbox.")
+
+    def send(self, inbox, data, **kwargs):
+        """Send the provided data to an inbox."""
+        if isinstance(data, dict) or isinstance(data, list):
+            self.__post_message(inbox, json.dumps(data), self.JSON_LD,
+                                **kwargs)
+        elif isinstance(data, str):
+            self.__post_message(inbox, data, self.JSON_LD, **kwargs)
+        elif isinstance(data, Graph):
+            ct = self.__accept_post_options(inbox, **kwargs) or self.JSON_LD
+            self.__post_message(inbox, data.serialize(format=ct), ct,
+                                **kwargs)
+        else:
+            raise TypeError(
+                    "You cannot send data of type {}.".format(type(data)))
